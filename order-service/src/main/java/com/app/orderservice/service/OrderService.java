12d9d95b8139196efc5dbe00866064daca62e273
Alex <alexandr.jelia@gmail.com>
2023-03-06 21:22:41 +0300

    implement own span

--- order-service/src/main/java/com/app/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/app/orderservice/service/OrderService.java
@@ -7,6 +7,9 @@ import com.app.orderservice.model.Order;
 import com.app.orderservice.model.OrderLineItems;
 import com.app.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.sleuth.Span;
+import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -18,10 +21,12 @@ import java.util.UUID;
 @Service
 @RequiredArgsConstructor
 @Transactional
+@Slf4j
 public class OrderService {
 
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
+    private final Tracer tracer;
 
     public String placeOrder(OrderRequest orderRequest) {
         List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList().stream()
@@ -34,23 +39,32 @@ public class OrderService {
         List<String> skuCodes = order.getOrderLineItemsList().stream()
                 .map(OrderLineItems::getSkuCode)
                 .toList();
+        log.info("Calling inventory service");
 
-        //make request like
-        //http://localhost:8082/api/inventory?skuCode=item1&skuCode=item2
-        //retrieve from response array of InventoryResponse objects
-        //which contain boolean isInStock
-        InventoryResponse[] responseArray = webClientBuilder.build().get()
-                .uri("http://inventory-service/api/inventory",
-                        uriBuilder -> uriBuilder.queryParam("skuCode",skuCodes).build())
-                .retrieve()
-                .bodyToMono(InventoryResponse[].class) //here request is async
-                .block();  //to make sync
-        boolean areProductsInStock = Arrays.stream(responseArray).allMatch(InventoryResponse::isInStock);
-        if(areProductsInStock) {
-            orderRepository.save(order);
-            return "Order Placed Successfully";
-        }else {
-            throw new IllegalArgumentException("Product is not in stock");
+        //Implement own span to trace this part of code
+        //Spring Cloud Sleuth will assign this spanId to below code
+        Span inventoryServiceLookup = tracer.nextSpan().name("InventoryServiceLookup");
+        try(Tracer.SpanInScope spanInScope = tracer.withSpan(inventoryServiceLookup.start())){
+
+            //make request like
+            //http://localhost:8082/api/inventory?skuCode=item1&skuCode=item2
+            //retrieve from response array of InventoryResponse objects
+            //which contain boolean isInStoc
+            InventoryResponse[] responseArray = webClientBuilder.build().get()
+                    .uri("http://inventory-service/api/inventory",
+                            uriBuilder -> uriBuilder.queryParam("skuCode",skuCodes).build())
+                    .retrieve()
+                    .bodyToMono(InventoryResponse[].class) //here request is async
+                    .block();  //to make sync
+            boolean areProductsInStock = Arrays.stream(responseArray).allMatch(InventoryResponse::isInStock);
+            if(areProductsInStock) {
+                orderRepository.save(order);
+                return "Order Placed Successfully";
+            }else {
+                throw new IllegalArgumentException("Product is not in stock");
+            }
+        }finally {
+            inventoryServiceLookup.end();
         }
     }
 
